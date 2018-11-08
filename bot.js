1863968e4deec63ed056d90f58bb402f68cb7b51
Meato <41707872+Meato@users.noreply.github.com>
2018-11-09 00:17:30 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,26 +1,47 @@
+const Discord = require('discord.js');
+const Client = new Discord.Client();
+const prefix = '+'
+
+Client.on('ready', () => {
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${Client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${Client.guilds.size} " ]`);
+  console.log(`Users! [ " ${Client.users.size} " ]`);
+  console.log(`channels! [ " ${Client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+});
+
+
+
 var ServerID = "510045258139303946"; 
 var ChannelID = "510195806079287316";
 
-const Discord = require('discord.js');
-const client = new Discord.Client();
-const prefix = '1';
-
-client.on('warn', console.warn);
-
-client.on('error', console.error);
-// Codes server - !.Zeyad' , ♚Roýale#5555
-
-
-client.on('ready', () => console.log('ProBot Credits Miner Discord.js Script','\n','By Zeyad,Royale','\n','ProBot credits miner is ready!'));// Codes Server` - iiZeyad , Roýale"1.5k 🍷#1111
-
-client.on('disconnect', () => console.log('PROBOT credits miner had disconnected!'));
-
-client.on('reconnecting', () => console.log('PROBOT credits miner is reconnecting...'));// Codes server - !.Zeyad' , ♚Roýale#5555
+var ServerID = "510045258139303946"; 
+var ChannelID = "510195806079287316";
+Client.on('warn', console.warn);
+Client.on('error', console.error);
+Client.on('disconnect', () => console.log('PROBOT credits miner had disconnected!'));
+Client.on('reconnecting', () => console.log('PROBOT credits miner is reconnecting...'));
 
 
 function timerFunc() {
-    client.on('message', msg => {
-        client.guilds.get(ServerID).channels.get(ChannelID).send(Math.random().toString(36).substring(7)) // Codes server - !.Zeyad' , ♚Roýale#5555
+    Client.on('message', msg => {
+        Client.guilds.get(ServerID).channels.get(ChannelID).send(Math.random().toString(36).substring(7)) 
 
 
     });
@@ -29,15 +50,4 @@ function timerFunc() {
 var timer = setTimeout(timerFunc, 500);
 
 
-
-
-//---------------------------------------------------------------------------------------------
-//---------------------------------------------------------------------------------------------
-//---------------------------------------------------------------------------------------------
-
-
-
-
-
-// THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);

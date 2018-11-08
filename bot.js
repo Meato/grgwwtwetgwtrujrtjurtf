46a17e9cceb5f8894829f000b64c9619ec60deda
Meato <41707872+Meato@users.noreply.github.com>
2018-11-08 14:39:07 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -30,34 +30,9 @@ client.on('ready', () => {
 
 
 
-client.on("message", async message => {
-	const myID = "248412206637514753";
-if (message.author.bot) return;
-    if (message.channel.type === "dm") return;
-    let prefix = "join";
-    let messageArray = message.content.split(" ");
-    let command = messageArray[0];
-
-if (command === `${prefix}join`) {
-        message.member.voiceChannel.join()
-    message.channel.send(' ')
-};
-
-});
-
-client.login(توكن حسابك);
-"NTA3MjM2MDgyNjU2NDc3MTg5.DsWo6Q.sFZN5hRMHzH13I_jSUqUWHYcjzQ"
-const Discord = require('discord.js');
-const client = new Discord.Client();
-
-client.on('ready', () => {
-  console.log(Logged in as ${client.user.tag}!);
-});
-
-
 client.on('ready', async() => {
-var server = "Id server"; 510045258139303946
-var channel = "id room";  510045318860242944
+var server = "510045258139303946"; 
+var channel = "510045318860242944";  
     setInterval(()=>{
     client.guilds.get(server).channels.get(channel).send('Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , ')
     },305);
